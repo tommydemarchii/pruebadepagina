73a36c2dfc152ee11e3e7f2c538763cfc4fc2d5d
tommydemarchii <118849028+tommydemarchii@users.noreply.github.com>
2022-11-22 16:11:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector("#yesBtn");
+
+yesBtn.addEventListener("click",function () {
+    alert("sabia que querias<3")
+});
+
+const noBtn = document.querySelector("#noBtn");
+
+noBtn.addEventListener("click",function() {
+    alert("sos un gil")
+});
